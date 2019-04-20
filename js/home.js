9725b548bd8eb465063474f4ed34d653f5eec8b1
reshma <reshmar@qburst.com>
2019-04-21 00:35:00 +0530

    home page in progress

--- js/home.js
+++ js/home.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
 	$('.home-slider').slick({
     	dots: true,
   	});
+
+	$('.home-partners-carousel').slick({
+    	dots: true,
+    	slidesToShow: 3,
+  		slidesToScroll: 3
+  	});
+
+  	$('.home-share-carousel').slick({
+    	dots: true,
+    	slidesToShow: 4,
+  		slidesToScroll: 3
+  	});
+
+  	
+  	
 });
