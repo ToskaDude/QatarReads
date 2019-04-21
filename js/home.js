5f9b2be00cec7e2e87c3f1285f9bdbb4a446ba94
reshma <reshmar@qburst.com>
2019-04-21 09:44:03 +0530

    home page completed

--- js/home.js
+++ js/home.js
@@ -1,18 +1,61 @@
 $(document).ready(function(){
 	$('.home-slider').slick({
     	dots: true,
+    	arrows: false
   	});
 
 	$('.home-partners-carousel').slick({
-    	dots: true,
+    	dots: false,
     	slidesToShow: 4,
-  		slidesToScroll: 3
+  		slidesToScroll: 3,
+  		arrows: true,
+  		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        centerMode: true,
+
+		      }
+    		},
+    		{
+		      breakpoint: 640,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+
+		      }
+    		},
+    	]
   	});
 
   	$('.home-share-carousel').slick({
-    	dots: true,
-    	slidesToShow: 5,
-  		slidesToScroll: 3
+    	dots: false,
+    	slidesToShow: 4,
+  		slidesToScroll: 3,
+  		arrows: true,
+  		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        centerMode: true,
+
+		      }
+    		},
+    		{
+		      breakpoint: 640,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		        centerMode: true,
+
+		      }
+    		},
+
+    	]
   	});
 
   	
